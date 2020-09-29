01d35081d4bdad7e89ebf55421028f24b33dc462
Pascal Hauser <pascal.hauser457@gmail.com>
2020-09-29 14:11:04 +0200

    add week 3

--- material/week2/ball/ball.js
+++ material/week2/ball/ball.js
@@ -1,7 +1,12 @@
 
 const radius = 10;
-const ball = {x:20, y:0, dx: 5, dy: 1};
+const ball = {x:20, y:20, dx: 5, dy: 1};
 let   old  = {x: ball.x, y: ball.y};
+
+const gravity = 1.2;
+let eneregy = 3;
+
+
 
 function start() {
     const canvas  = document.getElementById("canvas");
@@ -15,17 +20,33 @@ function start() {
 }
 
 function nextBoard() {
+    
     // keep old ball values for the sake of efficient clearing of the old display
+    old.x = ball.x;
+    old.y = ball.y;
 
-    // handle ball is hitting the bounds
-    //   reverse direction
+
+    // handle ball is hitting the bounds --> reverse direction
+    if(ball.y + ball.dy + radius > 400 || ball.y + ball.dy - radius < 0){
+        ball.dy *= -1;
+    }
+
+    if(ball.x + ball.dx + radius > 400 || ball.x + ball.dx - radius < 0){
+        ball.dx *= -1;
+    }
+
     //   lose some energy relative to the current inertia (only velocity varies)
-
+    //TODO:
     // calculate new position
+    if(old.y < ball.y){
+        energy += gravity * energy; 
+    }
+    ball.x += ball.dx;
+    ball.y += ball.dy * eneregy;
     // calculate any changes in velocity due to gravitational pull or medium resistance
-
-
+    
 }
+
 
 function display(context) {
     context.clearRect(old.x - radius - 1 , old.y - radius -1 , 22, 22 );
@@ -37,5 +58,4 @@ function fillBox(context) {
     context.arc(ball.x, ball.y, radius, 0, 6.3, false);
     context.fill();
 }
-
 
