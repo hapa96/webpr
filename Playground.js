344cc0b78beebb048b015c70a1b9e5dbe3ba7dc4
Pascal Hauser <pascal.hauser457@gmail.com>
2020-10-27 15:18:38 +0100

    before week 07

--- Playground.js
+++ Playground.js
@@ -1,12 +1,8 @@
-const factor_ = 10;
-const e = {revenue: 10000, bonus: null};
-const bonusCalculation = Function('x', `return x.bonus = x.revenue * ${factor_}`);
 
 
-bonusCalculation(e);
+
+
 
 
 //Test
-
-bonusCalculation(e);
-document.writeln (e.bonus === e.revenue * factor_);
+document.writeln ();
